9dfabcc8269d6f86a2c42e5ffe0ae85d83a56975
Mike B <michael.r.boyle@gmail.com>
2015-03-16 15:24:07 -0400

    added silent download of file

--- DVWA.sh
+++ DVWA.sh
@@ -1,4 +1,5 @@
 cd /var/www
 sudo mkdir demo
 cd /var/www/demo
-wget https://github.com/RandomStorm/DVWA/archive/v1.0.8.zip
+echo -e "\n--- creating a DVWA on LAP Server ---\n"
+wget https://github.com/RandomStorm/DVWA/archive/v1.0.8.zip  > /dev/null 2>&1
