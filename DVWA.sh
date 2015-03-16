0a4b7652da754018bedb400afadaa159772c8fcf
Mike B <michael.r.boyle@gmail.com>
2015-03-16 15:27:42 -0400

    added unzip download

--- DVWA.sh
+++ DVWA.sh
@@ -2,4 +2,6 @@ cd /var/www
 sudo mkdir demo
 cd /var/www/demo
 echo -e "\n--- creating a DVWA on LAP Server ---\n"
+apt-get -qq update
+apt-get -y install unzip > /dev/null 2>&1
 wget https://github.com/RandomStorm/DVWA/archive/v1.0.8.zip  > /dev/null 2>&1
